01dbe481690a4f3af1c674ade70225f024313443
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-01-02 14:29:07 -0500

    remove star import

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-use ncurses::*;
+use ncurses as nc;
 
 use hex_grid::HexGrid;
 use ascii_view::AsciiView;
@@ -38,31 +38,29 @@ struct NCurses;
 
 impl NCurses {
     pub fn new() -> NCurses {
-        initscr();
+        nc::initscr();
         NCurses
     }
 }
 
 impl Drop for NCurses {
     fn drop(&mut self) {
-        endwin();
+        nc::endwin();
     }
 }
 
 fn mainloop(contents : &Vec<u8>) {
     let _nc = NCurses::new();
-    keypad(stdscr, true);
-    // timeout(-1);
-    noecho();
-    curs_set( CURSOR_VISIBILITY::CURSOR_INVISIBLE );
+    nc::keypad(nc::stdscr, true);
+    nc::noecho();
+    nc::curs_set( nc::CURSOR_VISIBILITY::CURSOR_INVISIBLE );
 
-    start_color();
-    // init_pair(1, COLOR_RED, COLOR_BLACK);
-    init_pair(1, COLOR_WHITE, COLOR_GREEN);
+    nc::start_color();
+    nc::init_pair(1, nc::COLOR_WHITE, nc::COLOR_GREEN);
 
     let mut scr_x = 0;
     let mut scr_y = 0;
-    getmaxyx(stdscr, &mut scr_y, &mut scr_x);
+    nc::getmaxyx(nc::stdscr, &mut scr_y, &mut scr_x);
 
     // Layout: We leave 2 spaces between hex view and ascii view. Every byte
     // takes 3 characters in hex view and 1 character in ascii view. So we have
@@ -78,35 +76,35 @@ fn mainloop(contents : &Vec<u8>) {
                                          contents );
     ascii_view.draw();
 
-    refresh();
+    nc::refresh();
 
     loop {
-        let ch = getch();
+        let ch = nc::getch();
 
         if ch == 27 {
             break;
 
-        } else if ch == b'e' as i32 { // e
+        } else if ch == b'e' as i32 {
             grid.widen();
-            clear();
+            nc::clear();
             grid.draw();
             ascii_view.draw();
-            refresh();
+            nc::refresh();
 
-        } else if ch == b'q' as i32 { // q
+        } else if ch == b'q' as i32 {
             grid.narrow();
-            clear();
+            nc::clear();
             grid.draw();
             ascii_view.draw();
-            refresh();
+            nc::refresh();
 
         } else {
             if grid.keypressed(ch) {
                 ascii_view.move_cursor(grid.get_byte_idx());
-                clear();
+                nc::clear();
                 grid.draw();
                 ascii_view.draw();
-                refresh();
+                nc::refresh();
             }
         }
     }
